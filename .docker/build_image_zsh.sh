d3064d9e50366d9e609bf8876a70364a01828b48
vinceH <vinceHuyghe@users.noreply.github.com>
2022-10-21 23:49:48 +0200

    build

--- .docker/build_image_zsh.sh
+++ .docker/build_image_zsh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo -e "Building kvp_recon_ros:lastest 
+echo -e "Building kvp_recon_ros:lastest"
 
 docker build --pull --rm -f ./.docker/Dockerfile  -t kvp_recon_ros:latest .
